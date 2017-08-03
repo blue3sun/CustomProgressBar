71263675cd39b8426ba6a417581185f6dcd57cd2
zenglanjing <zenglanjing@bravowhale.com>
2017-08-03 15:03:12 +0800

    自定义的ProgressBar

---
+++ app/src/main/java/com/monkey/customprogressbar/CustomProgress.java
@@ -0,0 +1,26 @@
+package com.monkey.customprogressbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
+/**
+ * Description: 半圆形的进度条
+ * Author: lanjing
+ * Time: 2017/8/3 10:51
+ */
+
+public class CustomProgress extends ProgressBar {
+
+    public CustomProgress(Context context) {
+        super(context);
+    }
+
+    public CustomProgress(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomProgress(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
